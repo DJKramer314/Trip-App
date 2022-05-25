a9b5221c79599f543a653263dc159d1fb59824c7
DJKramer314 <djkramer314@gmail.com>
2022-05-24 22:08:50 -0500

    Initial Commit

---
+++ Shared/Trip_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trip_AppApp.swift
+//  Shared
+//
+//  Created by Dylan on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Trip_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
